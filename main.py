25cd028988903634a1cf035f9e49b2ab8af61732
Joao Marcello <joaomarcello.mm@gmail.com>
2025-07-14 18:16:44 -0300

    add file with use example

---
+++ main.py
@@ -0,0 +1,15 @@
+import numpy as np
+from fwa import FWA
+
+def sphere(x):
+    return np.sum(x ** 2)
+
+dim = 10
+bounds = [(-100, 100)] * dim
+
+fwa = FWA(func=sphere, dim=dim, bounds=bounds)
+fwa.load_prob(n=5, m=50, max_iter=100)
+fwa.run()
+
+print("Melhor solução:", fwa.best_solution)
+print("Valor:", fwa.func(fwa.best_solution))
