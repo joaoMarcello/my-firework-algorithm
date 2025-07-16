e28e24705fe8ca6486e6b7e577606b01e020d6a8
Luís Felipe <luisfrp741@gmail.com>
2025-07-16 12:31:27 -0300

    Benchmarks

--- run_fwa_nsp.sh
+++ run_fwa_nsp.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
 # Ativa o ambiente virtual no Windows (modo compatível com Git Bash ou WSL)
-source env/Scripts/activate
+# source env/Scripts/activate
+# conda activate fwa_paa
 
 # Executa o FWA para o Nurse Scheduling Problem com parâmetros definidos
 python main_nsp.py \
   --xml_path data/ORTEC01.xml \
   --save_file results_1/fwa_nsp_run_07 \
-  --fwa_n 20 \
+  --fwa_n 10 \
   --fwa_m 100 \
   --fwa_a 0.04 \
   --fwa_b 0.8 \
-  --fwa_a_hat 2.0 \
+  --fwa_a_hat 1.5 \
   --fwa_m_hat 10 \
-  --fwa_max_iter 4000 \
-  --fwa_select_mode distance
+  --fwa_max_iter 100 \
+  --fwa_select_mode roulette
 
 # Pausar o terminal após a execução
 read -p "Pressione [Enter] para sair..."
