7b6e0f71d347186968562d77cd219c11e706478f
Joao Marcello <joaomarcello.mm@gmail.com>
2025-07-17 23:11:57 -0300

    changes for new tests

--- run_fwa_nsp.sh
+++ run_fwa_nsp.sh
@@ -10,17 +10,17 @@
     # --xml_path benchmark/Instance2.xml \
 python main_nsp.py \
   --xml_path data/ORTEC01.xml \
-  --save_file results_tests_17-07/fwa_nsp_run_01 \
+  --save_file results_tests_17-07/fwa_nsp_run_02.for_real \
   --fwa_n 20 \
-  --fwa_m 30 \
+  --fwa_m 50 \
   --fwa_a 0.04 \
   --fwa_b 0.8 \
   --fwa_a_hat 1.5 \
   --fwa_m_hat 5 \
-  --fwa_j 0.2 \
-  --fwa_j_hat 0.5 \
-  --fwa_max_iter 500 \
+  --fwa_j 0 \
+  --fwa_j_hat 0 \
+  --fwa_max_iter 5000 \
   --fwa_select_mode roulette
 
-# Pausar o terminal após a execução
-read -p "Pressione [Enter] para sair..."
+# # Pausar o terminal após a execução
+# read -p "Pressione [Enter] para sair..."
